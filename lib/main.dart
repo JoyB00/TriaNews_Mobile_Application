1f99b8f1136704ae155ecd323114519f83baee71
JoyB00 <dendypramartha03@gmail.com>
2023-09-24 03:40:59 +0800

    profile 80%

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const MaterialApp(home: ProfileView());
   }
 }
