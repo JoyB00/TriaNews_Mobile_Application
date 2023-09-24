a6b63f19b729f857062345c3abe442a7e7453130
JoyB00 <dendypramartha03@gmail.com>
2023-09-24 16:57:45 +0700

    profile fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:news_pbp/View/profile.dart';
 
-import 'package:news_pbp/View/login.dart';
-import 'package:adaptive_theme/adaptive_theme.dart';
 void main() {
   runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-
-    return AdaptiveTheme(
-      light: ThemeData.light(),
-      dark: ThemeData.dark(),
-      //debugShowFloatingThemeButton: true, // <------ add this line
-      initial: AdaptiveThemeMode.light,
-      builder: (theme, darkTheme) => MaterialApp(
-        theme: theme,
-        darkTheme: darkTheme,
-        home: LoginView(),
-      ),
+    return const MaterialApp(
+      home: ProfileView(),
     );
   }
 }
