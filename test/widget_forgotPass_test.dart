1d90c494f634eab5769ecd1be226a1da210b6ba5
JoyB00 <dendypramartha03@gmail.com>
2023-12-09 20:47:25 +0700

    FINISH TUBES

--- test/widget_forgotPass_test.dart
+++ test/widget_forgotPass_test.dart
@@ -21,25 +21,31 @@ void main() {
       }),
     );
 
-await tester.dragUntilVisible(find.text("Forgot Password ?"),
+    await tester.dragUntilVisible(find.text("Forgot Password ?"),
         find.byType(SingleChildScrollView), const Offset(521.7, 592.3));
-        await Future.delayed(const Duration(seconds: 2));
+    await Future.delayed(const Duration(seconds: 2));
     await tester.tap(find.text("Forgot Password ?"));
     await Future.delayed(const Duration(seconds: 4));
     await tester.pumpAndSettle();
 
     await tester.enterText(find.byType(TextField).at(0), "joel@gmail.com");
+
+    await tester.dragUntilVisible(find.byType(ElevatedButton),
+        find.byType(SingleChildScrollView), const Offset(521.7, 592.3));
+    await Future.delayed(const Duration(seconds: 2));
     await tester.tap(find.byType(ElevatedButton));
     await Future.delayed(const Duration(seconds: 4));
     await tester.pumpAndSettle();
 
     await tester.enterText(find.byType(TextFormField).at(0), "234567");
     await tester.enterText(find.byType(TextFormField).at(1), "234567");
+    await tester.dragUntilVisible(find.byType(ElevatedButton),
+        find.byType(SingleChildScrollView), const Offset(521.7, 592.3));
+    await Future.delayed(const Duration(seconds: 2));
     await tester.tap(find.byType(ElevatedButton));
     await Future.delayed(const Duration(seconds: 4));
     await tester.pumpAndSettle();
     expect(find.byType(LoginView), findsWidgets);
-
   });
 
   // testWidgets("LoginPage failed", (WidgetTester tester) async {
